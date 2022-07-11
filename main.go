7fb2bf75cc3eaf01b5168467fdb3d9fae4eafb8b
Rodolfo Ribeiro <rodolfo.ribeiro@quero2-rodolfo.ribeiro>
2022-07-11 17:10:47 -0300

    Added script to create database

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func init() {
+	model.CreateDatabase()
 	model.ConnectDatabase()
 }
 
--- model/database.go
+++ model/database.go
@@ -1,6 +1,9 @@
 package model
 
 import (
+	"database/sql"
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -17,4 +20,64 @@ func ConnectDatabase() {
 	}
 
 	DB = db
+}
+
+func CreateDatabase() {
+	db, _ := sql.Open("mysql", "root:root@tcp(localhost:3306)/db_library")
+	err := db.Ping()
+	db.Close()
+	if err != nil {
+		db, _ := sql.Open("mysql", "root:root@tcp(localhost:3306)/")
+		createTables(db)
+		db.Close()
+		fmt.Println("Database created sucessfuly!")
+	} else {
+		fmt.Println("Database already exists!")
+	}
+}
+
+func createTables(db *sql.DB) {
+	execute(db, "CREATE DATABASE IF NOT EXISTS db_library;")
+
+	execute(db, "USE db_library;")
+
+	execute(db, `
+
+		CREATE TABLE books
+		(
+			id 				INTEGER,
+			name 			VARCHAR(80),
+			description 	VARCHAR(100),
+
+			CONSTRAINT PK_tb_books_id PRIMARY KEY (id)
+		);`,
+	)
+
+	execute(db, `CREATE TABLE categories
+		(
+			id 				INTEGER,
+			name 			VARCHAR(100),
+
+			CONSTRAINT PK_tb_categories_id PRIMARY KEY (id)
+		);`,
+	)
+	execute(db, `CREATE TABLE bookshelves
+		(
+			book 				INTEGER,
+			category 			INTEGER,
+
+			CONSTRAINT PK_tb_bookshelf_book_category PRIMARY KEY (book, category),
+			CONSTRAINT FK_tb_bookshelf_tb_books_id FOREIGN KEY (book)
+				REFERENCES books (id),
+			CONSTRAINT FK_tb_bookshelf_tb_category_id FOREIGN KEY (category)
+				REFERENCES categories (id)
+		);`,
+	)
+}
+func execute(db *sql.DB, sql string) sql.Result {
+	result, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
+	return result
 }
